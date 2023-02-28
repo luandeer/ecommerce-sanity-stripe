8fdb4d427af493538429fdda63da87d632dd1328
alexander <alexanderperezreginaldo@gmail.com>
2023-02-28 11:09:42 -0500

    instalado sanity

---
+++ sanity-ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'i4lx0sj3',
+    dataset: 'production'
+  }
+})
---
+++ sanity-ecommerce/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sanity-ecommerce',
+
+  projectId: 'i4lx0sj3',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
